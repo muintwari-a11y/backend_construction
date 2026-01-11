072e7c7706002fe4258830caa66b36b490873389
OLIVIER NTWARI <olivierntwari@OLIVIERs-MacBook-Pro.local>
2026-01-11 17:36:30 +0200

    Update main.py to detect PORT and change render startCommand to python app/main.py

--- app/main.py
+++ app/main.py
@@ -33,5 +33,7 @@ def startup_event():
     create_tables()
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
